aa97bf0d0d259d2de1cb0a986b4381776aaaf9ed
MarioSalinas <mario_1605@live.com.mx>
2019-01-15 16:57:37 -0600

    functions to find order ordersPage

--- src/pages/orders/orders.ts
+++ src/pages/orders/orders.ts
@@ -21,7 +21,8 @@ export class OrdersPage {
     this.orderProvider.getProducts().valueChanges().subscribe((data) => {
       this.orders = data;
       console.log(this.orders)
-      console.log(`Coordinates Lat: ${gpsProvider.lat} Lon: ${gpsProvider.lon}`)
+      this.loadData()
+      //console.log(`Coordinates Lat: ${gpsProvider.lat} Lon: ${gpsProvider.lon}`)
     }, (error) => {
       console.log(error);
     })
@@ -29,6 +30,42 @@ export class OrdersPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrdersPage');
+  }
+
+  loadData(){
+    for (let order in this.orders) {
+        // onsole.log(this.orders[order].id)
+        if (this.inRange(this.orders[order].markerlatlong, 10)) {
+            this.orders[order].status = "ready"
+            return true
+        }
+    }
+    //console.log(this.orders)
+  }
+
+  inRange(marker, distance){
+    if (this.CalcDistanceBetween(this.gpsProvider.lat, this.gpsProvider.lon, marker.lat, marker.lng) < distance) {
+      console.log(`Distancia: ${this.CalcDistanceBetween(this.gpsProvider.lat, this.gpsProvider.lon, marker.lat, marker.lng)}`)
+      return true
+    }
+  }
+
+  CalcDistanceBetween(lat1, lon1, lat2, lon2) {
+    //Radius of the earth in:  1.609344 miles,  6371 km  | var R = (6371 / 1.609344);
+    var R = 3958.7558657440545; // Radius of earth in Miles
+    var dLat = this.toRad(lat2-lat1);
+    var dLon = this.toRad(lon2-lon1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(this.toRad(lat1)) * Math.cos(this.toRad(lat2)) *
+            Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+    return d;
+  }
+
+  toRad(Value) {
+      /** Converts numeric degrees to radians */
+      return Value * Math.PI / 180;
   }
 
 }
