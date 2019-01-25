9e905d0f5102341a1423ac81d4f5d6b363721546
MarioSalinas <mario_1605@live.com.mx>
2019-01-25 12:53:34 -0600

    get orders in process

--- src/pages/in-process/in-process.ts
+++ src/pages/in-process/in-process.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StoreProvider } from '../../providers/store/store';
+import { Storage } from '@ionic/storage';
 
 /**
  * Generated class for the InProcessPage page.
@@ -14,12 +16,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'in-process.html',
 })
 export class InProcessPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  store
+  list: any = []
+  constructor(public navCtrl: NavController,
+    public storeProvider: StoreProvider,
+    private storage: Storage,
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InProcessPage');
+    this.storage.get('store').then((data)=>{
+      console.log(data)
+      if(data){
+        this.store = data
+        this.storeProvider.getOrderProcess(data.uid).valueChanges().subscribe((sdata)=>{
+          console.log(sdata)
+          if(sdata){
+            this.list = sdata
+          }
+        })
+      }
+    })
   }
+
 
 }
