8832fc4f00a9eb151fdc9d1cde94940a463dc572
MarioSalinas <mario_1605@live.com.mx>
2019-01-28 07:11:18 -0600

    logout

--- src/pages/information/information.ts
+++ src/pages/information/information.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 import { RecordProvider } from '../../providers/record/record';
+import { AlertController } from 'ionic-angular';
+import { AuthenticationProvider } from '../../providers/authentication/authentication';
+import { LoginPage } from '../login/login';
 
 /**
  * Generated class for the InformationPage page.
@@ -21,6 +24,8 @@ export class InformationPage {
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private recordProvider: RecordProvider,
+    private alertCtrl: AlertController,
+    private authenticationProvider: AuthenticationProvider,
     private storage: Storage) {
   }
 
@@ -41,6 +46,33 @@ export class InformationPage {
         })
       }
     })
+  }
+
+  logOut(){
+    let alert = this.alertCtrl.create({
+      title: 'Fue un gusto atenderte!',
+      subTitle: 'Esperamos que vuelvas',
+      buttons: ['Ok']
+    });
+    let alertError = this.alertCtrl.create({
+      title: 'Ups!',
+      subTitle: 'Hubo un error',
+      buttons: ['Ok']
+    });
+
+    this.authenticationProvider.logOut()
+    .then(
+      (data)=>{
+        alert.present();
+        console.log(data)
+        this.navCtrl.setRoot(LoginPage);
+      }
+    ).catch(
+      (error)=>{
+        alertError.present();
+        console.log(error)
+      }
+    )
   }
 
 }
